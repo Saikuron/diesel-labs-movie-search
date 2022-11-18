5b294e615ec8bf9e4bafc67d81d96c46d6e899ee
j <demalliard.jean@gmail.com>
2022-11-18 19:46:07 +0100

    Add some tests

--- backend/lib/test_back.py
+++ backend/lib/test_back.py
@@ -1,5 +1,9 @@
-from app import open_file
+from app import open_file, whole_names
 
 def test_open_file():
     test_file = open_file("data/links.csv")
     assert len(test_file) > 0
+    assert len(test_file[1]) == 3
+
+def test_whole_names():
+    assert type(whole_names[1]) == str
